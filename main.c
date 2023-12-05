a08fc7751b9db8c032b4c9a65a4b2174686db3de
Taylan Özer <s85500@htw-dresden.de>
2023-12-05 20:19:48 +0100

    Anpassung plus Hinzufügen .gitignore

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 
-    struct Aktien {
+    /* struct Aktien {
 
         char isin;
         char name_aktien;
@@ -15,32 +15,11 @@
         char kaeufer_verkaeuefer_aktien;
    
     };
-
+    */
     
     //Erstellt ein Alias für das Struct um so nicht erneut das Struct auflisten zu müssen
-    typedef struct Aktien aktien;
+    //typedef struct Aktien aktien;
 
-//Aufgabe 1
-    void bubblesort(aktien Feld[], int n){
-        int i, j;
-
-        aktien temp;
-
-        for(i = 0; i < n - 1; i++){
-            for(j = i + 1; j < n; j++){
-                if(strcmp(Feld[i].name_aktien, Feld[j].name_aktien) > 0){
-                    temp = Feld[i];
-                    Feld[i] = Feld[i];
-                    Feld[j] = temp;
-
-                }
-
-
-            }
-
-        }
-
-    }
 
 //Aufgabe 2.) Funktion-Dividendenrendite
 void dividendenrendite(float akt_aktienkurs, float akt_dividende){
@@ -51,7 +30,7 @@ void dividendenrendite(float akt_aktienkurs, float akt_dividende){
 }
 //Aufgabe 3.) CSV-Datei zeilenweise einlesen mit verschiedenen Datentypen
 //Vereint mehrere Datentypen in einem
-union data{
+typedef union data{
 
     char isin;
     char name_aktien;
@@ -61,11 +40,35 @@ union data{
     char kaufdatum_verkaufdatum_aktien;
     char kaeufer_verkaeuefer_aktien;
 
-};
+}data;
 
-void schreibeCSV(FILE* stocks, union data* data){
+
+//Aufgabe 1
+    void bubblesort(data stocks[], int n){
+        int i, j;
+
+        data temp;
+
+        for(i = 0; i < n - 1; i++){
+            for(j = i + 1; j < n; j++){
+                if(strcmp(stocks[i].name_aktien, stocks[j].name_aktien) > 0){
+                    temp = stocks[i];
+                    stocks[i] = stocks[i];
+                    stocks[j] = temp;
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+
+void schreibeCSV(FILE* datei, data* data){
     for(int i = 0; i < sizeof(data); i++){
-        fprintf(stocks, "%s, %s, %d, %d, %f, %s, %s", data[i].isin, data[i].name_aktien, 
+        fprintf(datei, "%s, %s, %d, %d, %f, %s, %s", data[i].isin, data[i].name_aktien, 
             data[i].aktienkaeufe_verkaeufe, data[i].aktienkurs_kauf_verkauf, 
             data[i].jahresdividende, data[i].kaufdatum_verkaufdatum_aktien, 
             data[i].kaeufer_verkaeuefer_aktien);    
@@ -92,7 +95,7 @@ int main(void){
 
 
     //Bezieht sich auf das Struct Aktien
-    aktien Feld[] = {
+    data stocks[] = {
 
         {"DE000BASF111", "BASF AG", 100, 42, 3.30, "20.10.2023", "Klaus"},
         {"DE000BASF111", "BASF AG", -20, 50, 3.30, "03.11.2023", "Klaus"},
@@ -101,58 +104,78 @@ int main(void){
 
     };
 
-    //Gibt die Größe des Arrays zurück
-    int n = sizeof(Feld) / sizeof(Feld[0]); // Für Feld
+    
+    char antwort = toUpper(antwort);
 
-    //Sortierung der im Feld stehenden Elemente
-    bubblesort(Feld, n);
-
+    
     //Aufgabe 3.) CSV-Datei zeilenweise einlesen
-    FILE* stocks_pfeil = fopen("stocks.txt","w");
+    FILE* stocks_file = fopen("stocks.txt","w");
 
-    //Prüfung ob stocks geöffnet wurde
-    if(stocks_pfeil == NULL){
+    //Prüfung ob Datei leer
+    if(stocks_file == NULL){
         printf("Konnte Datei nicht öffnen.");
         return 0;
+        
+    }    
+
+    printf("Bitte auswählen: (1) Schreiben in Datei\t(2) nicht schreiben\t(3) Erweitern\t(4) Aktieninformationen auflisten\t(5) EXTRA: Dividendenrendite berechnen\n");
+    scanf("%s", &antwort);
+
+    if(antwort == '1'){
+
+        schreibeCSV(stocks_file, stocks);
+        fclose(stocks_file);
+
+    }else if(antwort == '2'){
+        printf("Ok.\n");
+        
     }
+    else if(antwort == '3'){
+        FILE* stocks_file = fopen("stocks.txt","a");
+        schreibeCSV(stocks_file, stocks);
+        fclose(stocks_file);
 
-    int n_csv = sizeof(stocks) / sizeof(stocks[0]); //Für CSV
-    char antwort;
-    char erweitern;
+    }else if(antwort == '4'){
+        printf("------Aktieninformationen------\n");
+
+         //Gibt die Größe des Arrays zurück
+        int n = sizeof(stocks) / sizeof(stocks[0]); // Für Feld
+
+        bubblesort(stocks, n);
+
+        for(int i = 0; i < n; i++){
+
+        printf("ISIN: %s", stocks[i].isin);
+        printf("Unternehmen: %s", stocks[i].name_aktien); 
+        printf("Anteile im Besitz: %d", stocks[i].aktienkaeufe_verkaeufe);  
+        printf("Aktienkurs: %d", stocks[i].aktienkurs_kauf_verkauf); 
+        printf("Jahresdividende: %f", stocks[i].jahresdividende);
+        printf("Kaufdatum/Verkaufdatum: %s", stocks[i].kaufdatum_verkaufdatum_aktien);
+        printf("Kaeufer/Verkaeufer: %s", stocks[i].kaeufer_verkaeuefer_aktien);
+        
+        }
+
+    }else if(antwort == '5'){
+
+        printf("\nEXTRA:-----------------------------------\n");
+        printf("------Berechnung Dividendenrendite------\n");
+
+        float akt_aktienkurs, akt_jahresdividende;
+
+        printf("\nBitte gib den Aktienkurs an: ");
+        scanf("%f", &akt_aktienkurs);
+
+        printf("\nBitte gib die Jahresdividende an: ");
+        scanf("%f", &akt_jahresdividende);
+
+        dividendenrendite(akt_aktienkurs, akt_jahresdividende);
     
+        
+    }else
 
-    printf("Willst Du etwas in eine Datei schreiben: (J)a oder (N)ein?");
-        if(antwort == 'J'){
-            schreibeCSV(stocks, n_csv);
-            
-        }else if(antwort == 'N'){
-            printf("\nOk.");
+        printf("\nBitte nur gueltige Werte eingeben!");
 
-        }else
-            printf("\nBitte nur gültige Werte eingeben!");
-
-        printf("\nWillst Du Deine Datei erweitern: (J)a oder (N)ein?");
-            if(erweitern == 'J'){
-                 FILE* stocks = fopen("stocks.txt","a");
-            }else
-                printf("\nOk.");
-
-
-    printf("\nEXTRA:-----------------------------------\n");
-    printf("------Berechnung Dividendenrendite------\n");
     
-    float akt_aktienkurs, akt_jahresdividende;
-
-    printf("\nBitte gib den Aktienkurs an: ");
-    scanf("%f", &akt_aktienkurs);
-
-    printf("\nBitte gib die Jahresdividende an: ");
-    scanf("%f", &akt_jahresdividende);
-
-    dividendenrendite(akt_aktienkurs, akt_jahresdividende);
-
-
-
     return 0;
 
 
